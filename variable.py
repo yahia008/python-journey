ec6e3a9f72f855bc09e90de88b26c8e486e1b72b
yahi008 <yahyatijjani99@gmail.com>
2023-12-20 04:01:18 -0800

    var1

---
+++ variable.py
@@ -0,0 +1,14 @@
+# A variable is a name or data that can change 
+
+x = 10 # assigning 10 to x
+
+y = 20 # assigning 10 to x
+
+a = x + y # summing the two variable together
+
+# unlike c u dont need to declare a variable b4 using
+
+print(a)
+x = 40 
+
+print(x+y)
